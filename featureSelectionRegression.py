1ca95db71a8236f595b62b4c7ccc2b4086b44200
kapalk <kasperi.palkama@icloud.com>
2016-11-03 12:53:17 +0200

    Update featureSelectionRegression.py

--- featureSelectionRegression.py
+++ featureSelectionRegression.py
@@ -3,11 +3,11 @@ import numpy as np
 
 def featureSelectionRegression(X,y,k):
     '''
-    -calculates cross correlataion between each feature and target
+    -calculates cross correlataion between each feature (X) and target (y)
     
     -converts correlation to F score and then to p-value
     
-    -selects k features with highest p-value and returns their correspongin indices
+    -selects k features with highest p-value and return their correspongin indices
     '''
     F, p_values = f_regression(X,y)
     bestFeaturesIdxs = p_values.argsort()[-k:][::-1]
