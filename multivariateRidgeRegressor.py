979ebc2e3d823bbaec9d89fbf852969a86b1ed02
kapalk <kasperi.palkama@icloud.com>
2016-11-05 17:28:44 +0200

    Add files via upload

---
+++ multivariateRidgeRegressor.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def fit(X,y,alpha):
+    '''
+    - NOTICE! remember to add bias term to X
+    
+    - X = features, y = targets, alpha = regulation scaling coefficient
+    
+    - calculates vector which minimizes the error function of multivariate regression
+    
+    - regulates complexity using L_2 norma as regulation parameter
+    
+    - returns optimal regression vector
+    
+    '''
+    Xt = np.transpose(X)
+    lambdaIdentity = alpha*np.identity(len(Xt))
+    inverse = np.linalg.inv(Xt.dot(X)+lambdaIdentity)
+    w_opt = np.dot(np.dot(inverse, Xt), y)
+    return w_opt
+
+
+
